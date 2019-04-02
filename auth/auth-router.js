91ccabcd04f7990c11fdeb92c8cd6998353f4a98
Trip4077 <bj2336@email.vccs.edu>
2019-04-02 11:16:40 -0400

    Auth /w JWT

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,28 @@
+require('dotenv').config();
+
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
+
+//Token Function
+const generateToken = user => {
+  //Info you want
+  const payload = {
+    subject: user.id,
+    username: user.username
+  }
+  //Token Configuration
+  const options = {
+    expiresIn: '1d',
+  }
+
+  const secret = process.env.secret;
+
+  //Return token
+  return jwt.sign(payload, secret, options)
+}
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
@@ -25,8 +46,13 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        //Generate Token
+        const token = generateToken(user);
+
+        //Send back token
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -37,4 +63,5 @@ router.post('/login', (req, res) => {
     });
 });
 
-module.exports = router;
+module.exports = router
+;
--- users/users-router.js
+++ users/users-router.js
@@ -6,7 +6,8 @@ const restricted = require('../auth/restricted-middleware.js');
 router.get('/', restricted, (req, res) => {
   Users.find()
     .then(users => {
-      res.json(users);
+      //View decoded token -- not for production
+      res.json({users, token: req.payload});
     })
     .catch(err => res.send(err));
 });
